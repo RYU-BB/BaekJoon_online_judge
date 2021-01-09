6e063605cb0d2b902e760dd43156d01ac37e775c
RYU-BB <tmxkdd5790@gmail.com>
2021-01-09 20:47:42 +0900

    brute force

---
+++ problem1476/problem1476.py
@@ -0,0 +1,21 @@
+input_e, input_s, input_m = map(int, input().split())
+e, s, m = 1, 1, 1
+year = 1
+
+while True:
+    if e == input_e and s == input_s and m == input_m:
+        print(year)
+        break
+
+    e += 1
+    s += 1
+    m += 1
+
+    if e == 16:
+        e = 1
+    if s == 29:
+        s = 1
+    if m == 20:
+        m = 1
+
+    year += 1
