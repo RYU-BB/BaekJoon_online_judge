3b46adb6e3902321758a29b6af95dc82c4fa7605
RYU-BB <tmxkdd5790@gmail.com>
2021-01-26 02:27:05 +0900

    problem15651

---
+++ problem15651/problem15651.py
@@ -0,0 +1,19 @@
+def go(index):
+    if index == m:
+        print(*array)
+        return
+
+    for i in range(1, n + 1):
+        check[i] = True
+        array.append(i)
+        go(index + 1)
+
+        array.pop()
+        check[i] = False
+
+
+n, m = map(int, input().split())
+check = [False for _ in range(n + 1)]
+array = []
+
+go(0)
