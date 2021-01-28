bf806ab2970a513ee0e65ff847d8f624bd87b966
RYU-BB <tmxkdd5790@gmail.com>
2021-01-29 06:43:50 +0900

    problem15656

---
+++ problem15656/problem15656.py
@@ -0,0 +1,21 @@
+def go(index):
+    if index == m:
+        print(*array)
+        return
+
+    for i in range(n):
+        check[i] = True
+        array.append(num_list[i])
+        go(index + 1)
+
+        array.pop()
+        check[i] = False
+
+
+n, m = map(int, input().split())
+num_list = list(map(int, input().split()))
+num_list.sort()
+check = [False for _ in range(n + 1)]
+array = []
+
+go(0)
